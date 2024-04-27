fff3b022b0e77b5e2df82d13b8873a3d44708329
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-04-27 18:20:10 +0100

    Basic implementation of Lloyd's algorithm

--- sleekit/codebooks.py
+++ sleekit/codebooks.py
@@ -53,3 +53,41 @@ class Codebook:
         probs = self.probabilities(data)
         probs = probs[probs > 0]
         return -np.sum(probs * np.log2(probs))
+
+    def mse(self, data):
+        """
+        Return the mean squared error of the data with this codebook.
+        """
+        quant = self.quantize_value(data)
+        return np.mean(np.square(data - quant))
+
+    def centroids(self, data):
+        """
+        Return the centroid for each codeword in the data.
+        """
+        labels = self.quantize_index(data)
+        ret = []
+        for k, v in enumerate(self.values):
+            d = data[labels == k]
+            ret.append(v if len(d) == 0 else d.mean())
+        return np.array(ret)
+
+
+def lloyd(data, codebook_size, lagrange_mult=0.0, max_iter=100, tol=1e-6):
+    """
+    Lloyd's algorithm for scalar quantization.
+    Returns a codebook that minimizes a combination of the mse and the etropy.
+    """
+    assert data.ndim == 1
+    # Initialize the codebook
+    data_range = data.max() - data.min()
+    codebook = Codebook(np.linspace(data.min(), data.max(), codebook_size))
+    for i in range(max_iter):
+        # Assign each data point to the nearest codeword
+        labels = codebook.quantize_index(data)
+        # Update each codeword to the centroid or its datapoints
+        new_codebook = Codebook(codebook.centroids(data))
+        if np.allclose(new_codebook.values, codebook.values, atol=tol * data_range):
+            break
+        codebook = new_codebook
+    return codebook
--- tests/test_codebooks.py
+++ tests/test_codebooks.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sleekit.codebooks import Codebook
+from sleekit.codebooks import lloyd
 
 
 def test_quant():
@@ -11,3 +12,10 @@ def test_quant():
     assert np.allclose(
         cb.quantize_value([0.1, 0.6, 0.9, -10, 0.4, 2]), [0.0, 0.5, 1.0, 0.0, 0.5, 1.0]
     )
+
+
+def test_lloyd():
+    data = np.random.randn(1000)
+    cb = lloyd(data, 8)
+    assert len(cb.values) == 8
+    assert len(cb.limits) == 7
