5374be7328956e79b4de5ee467a00a28fd5aa325
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-04-29 11:46:37 +0100

    Implementation of Lloyd-Max algorithm for entropy-constrained quantization

--- sleekit/codebooks.py
+++ sleekit/codebooks.py
@@ -18,14 +18,26 @@ class Codebook:
             self.limits = (self.values[:-1] + self.values[1:]) / 2
         self.check()
 
+    def clone(self):
+        """
+        Return a copy of the codebook.
+        """
+        return Codebook(self.values.copy(), self.limits.copy())
+
     def check(self):
         assert self.values.ndim == 1
         assert self.values.size > 0
-        assert sorted(self.values)
+        assert np.isfinite(self.values).all()
+        assert (self.values[1:] > self.values[:-1]).all()
         assert self.limits.ndim == 1
         assert self.limits.size == self.values.size - 1
+        assert np.isfinite(self.limits).all()
+        assert (self.limits[1:] > self.limits[:-1]).all()
         assert (self.limits >= self.values[:-1]).all()
         assert (self.limits <= self.values[1:]).all()
+
+    def __len__(self):
+        return len(self.values)
 
     def quantize_index(self, data):
         """
@@ -69,13 +81,35 @@ class Codebook:
         ret = []
         for k, v in enumerate(self.values):
             d = data[labels == k]
-            ret.append(v if len(d) == 0 else d.mean())
+            if len(d) != 0:
+                # Typical case: we use the centroid
+                ret.append(d.mean())
+            else:
+                # Edge case: no data point in the bin
+                if k == 0:
+                    ret.append(self.limits[0] - 1.0e-6)
+                elif k == len(self.values) - 1:
+                    ret.append(self.limits[-1] + 1.0e-6)
+                else:
+                    ret.append((self.limits[k - 1] + self.limits[k]) / 2)
         return np.array(ret)
 
+    def improve(self, data, lagrange_mult=0.0):
+        v = self.values
+        mid_points = (v[:-1] + v[1:]) / 2
+        if lagrange_mult != 0.0:
+            l = -np.log2(self.probabilities(data))
+            penalty = (l[1:] - l[:-1]) / (v[1:] - v[:-1])
+            self.limits = mid_points + lagrange_mult * penalty / 2
+        else:
+            self.limits = mid_points
+        self.values = self.centroids(data)
+        self.check()
 
-def lloyd(data, codebook_size, lagrange_mult=0.0, max_iter=100, tol=1e-6):
+
+def lloyd_max(data, codebook_size, lagrange_mult=0.0, max_iter=100, tol=1e-6):
     """
-    Lloyd's algorithm for scalar quantization.
+    Lloyd-Max algorithm for scalar quantization.
     Returns a codebook that minimizes a combination of the mse and the etropy.
     """
     assert data.ndim == 1
@@ -84,9 +118,9 @@ def lloyd(data, codebook_size, lagrange_mult=0.0, max_iter=100, tol=1e-6):
     codebook = Codebook(np.linspace(data.min(), data.max(), codebook_size))
     for i in range(max_iter):
         # Assign each data point to the nearest codeword
-        labels = codebook.quantize_index(data)
         # Update each codeword to the centroid or its datapoints
-        new_codebook = Codebook(codebook.centroids(data))
+        new_codebook = codebook.clone()
+        new_codebook.improve(data, lagrange_mult)
         if np.allclose(new_codebook.values, codebook.values, atol=tol * data_range):
             break
         codebook = new_codebook
--- tests/test_codebooks.py
+++ tests/test_codebooks.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sleekit.codebooks import Codebook
-from sleekit.codebooks import lloyd
+from sleekit.codebooks import lloyd_max
 
 
 def test_quant():
@@ -12,10 +12,18 @@ def test_quant():
     assert np.allclose(
         cb.quantize_value([0.1, 0.6, 0.9, -10, 0.4, 2]), [0.0, 0.5, 1.0, 0.0, 0.5, 1.0]
     )
+    assert len(cb) == 3
 
 
 def test_lloyd():
     data = np.random.randn(1000)
-    cb = lloyd(data, 8)
+    cb = lloyd_max(data, 8)
+    assert len(cb.values) == 8
+    assert len(cb.limits) == 7
+
+
+def test_lloyd_max():
+    data = np.random.randn(1000)
+    cb = lloyd_max(data, 8, 0.1)
     assert len(cb.values) == 8
     assert len(cb.limits) == 7
