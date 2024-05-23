0fe621d7ca6092548fd28aade78ab103d7e727d9
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-05-23 15:49:11 +0100

    Experiments to measure correction

--- trial.py
+++ trial.py
@@ -5,7 +5,7 @@ from sleekit.scaling import *
 import matplotlib.pyplot as plt
 import glob
 
-print("Layer MSE MSE+corr MSE+chol MSE+chol+corr MSE+inv MSE+inv+corr")
+print("Layer Base+MSE Base+CorrMSE Corr+MSE Corr+CorrMSE")
 for hessian_name in sorted(glob.glob("data/**.quant_hessian.npy")):
     name = hessian_name.replace("model.decoder.layers.", "")
     name = name.replace(".quant_hessian.npy", "")
@@ -28,21 +28,35 @@ for hessian_name in sorted(glob.glob("data/**.quant_hessian.npy")):
     cb = Codebook.uniform(17, -8, 8)
     sc_mse = compute_min_mse_scaling(weight, cb, 0, min_factor=0.5, grid_size=10)
     q_mse = quantize_with_scaling(weight, sc_mse, cb, H=H)
+    err_mse = quantization_error(q_mse, weight, H)
+    print(f"{err_mse}", end=" ")
     err_mse = quantization_error(q_mse, weight, removed_H)
     print(f"{err_mse}", end=" ")
     q_mse = quantize_with_scaling(weight, sc_mse, cb, H=removed_H)
+    err_mse = quantization_error(q_mse, weight, H)
+    print(f"{err_mse}", end=" ")
     err_mse = quantization_error(q_mse, weight, removed_H)
     print(f"{err_mse}", end=" ")
-    q_mse = quantize_with_scaling(weight, sc_mse, cb, H=H, act_order=2)
-    err_mse = quantization_error(q_mse, weight, removed_H)
-    print(f"{err_mse}", end=" ")
-    q_mse = quantize_with_scaling(weight, sc_mse, cb, H=removed_H, act_order=2)
-    err_mse = quantization_error(q_mse, weight, removed_H)
-    print(f"{err_mse}", end=" ")
-    q_mse = quantize_with_scaling(weight, sc_mse, cb, H=H, act_order=3)
-    err_mse = quantization_error(q_mse, weight, removed_H)
-    print(f"{err_mse}", end=" ")
-    q_mse = quantize_with_scaling(weight, sc_mse, cb, H=removed_H, act_order=3)
-    err_mse = quantization_error(q_mse, weight, removed_H)
-    print(f"{err_mse}", end=" ")
+    print()
+    continue
+
+    output_scaled = apply_scaling(weight, sc_mse, 0)
+    scale_input = compute_norm_scaling(output_scaled, 1)
+    scaled_weight = apply_scaling(weight, scale_input, 1)
+    scaled_H = np.expand_dims(scale_input, 0) * H * np.expand_dims(scale_input, 1)
+
+    sc_scale = compute_min_mse_scaling(scaled_weight, cb, 0, min_factor=0.5, grid_size=10, H=H)
+    q_scale = quantize_with_scaling(scaled_weight, sc_scale, cb, H=scaled_H)
+    err_scale = quantization_error(q_scale, scaled_weight, scaled_H)
+    print(f"{err_scale}", end=" ")
+
+    output_scaled = apply_scaling(weight, sc_mse, 0)
+    scale_input = compute_min_mse_scaling(output_scaled, cb, 1, min_factor=0.5, grid_size=100)
+    scaled_weight = apply_scaling(weight, scale_input, 1)
+    scaled_H = np.expand_dims(scale_input, 0) * H * np.expand_dims(scale_input, 1)
+
+    sc_scale = compute_min_mse_scaling(scaled_weight, cb, 0, min_factor=0.5, grid_size=10, H=H)
+    q_scale = quantize_with_scaling(scaled_weight, sc_scale, cb, H=scaled_H)
+    err_scale = quantization_error(q_scale, scaled_weight, scaled_H)
+    print(f"{err_scale}", end=" ")
     print()
