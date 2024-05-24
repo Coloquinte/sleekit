73366c1fbaf57bbd48caefef63bc33a394aa385e
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-05-24 20:55:11 +0100

    Experiment around optimal codebooks

--- codebook.py
+++ codebook.py
@@ -7,31 +7,29 @@ import glob
 import sys
 
 data = []
-for weight_name in sorted(glob.glob("data/**.weight.npy")):
-    name = weight_name.replace("data/model.decoder.layers.", "")
-    name = name.replace(".weight.npy", "")
-    if not "fc" in name and not "proj" in name:
-        continue
-    print(name)
-    weight = np.load(weight_name).astype(np.float32)
-    hessian_name = weight_name.replace("weight", "quant_hessian")
-    hessian = np.load(hessian_name).astype(np.float32)
-    sc = compute_norm_scaling(weight)
-    apply_scaling_in_place(weight, sc)
-    sc = compute_norm_scaling(weight, 1)
-    coeff = np.corrcoef(np.diag(hessian), np.square(sc))[0, 1]
-    print(f"Coefficient: {coeff}")
-    data.append(np.reshape(weight, (-1,)))
-    #plt.hist(np.reshape(weight, (-1,)), 100)
-    #plt.show()
+sz = 16
+cb = Codebook.uniform(sz, -1, 1)
 
-all_data = np.concatenate(data)
-plt.hist(all_data, 1000)
-plt.show()
-sys.exit(0)
+for i in range(10):
+    for weight_name in sorted(glob.glob("data/**.weight.npy")):
+        name = weight_name.replace("data/model.decoder.layers.", "")
+        name = name.replace(".weight.npy", "")
+        if not "fc" in name and not "proj" in name:
+            continue
+        #print(name)
+        weight = np.load(weight_name).astype(np.float32)
+        hessian_name = weight_name.replace("weight", "quant_hessian")
+        hessian = np.load(hessian_name).astype(np.float32)
+        sc = compute_min_mse_scaling(weight, cb, grid_size=10)
+        apply_scaling_in_place(weight, sc)
+        data.append(np.reshape(weight, (-1,)))
 
-for lagrange_mult in [0.0, 0.001]:
-    cb = lloyd_max(all_data, 256, lagrange_mult)
-    print(cb.values)
-    print(cb.entropy(all_data))
+    all_data = np.concatenate(data)
+    cb = lloyd_max(all_data, sz, sample_count=1000)
+    print(cb.values / max(abs(cb.values[0]), abs(cb.values[-1])))
+
+    plt.hist(all_data, 10000)
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.vlines(cb.values, ymin, ymax, color='red')
+    plt.show()
 
