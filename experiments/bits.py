63e72644933821fff81736ce4cc6521baa1a4a7f
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-06-06 12:34:12 +0100

    Add graphic of bitwidth impact

---
+++ experiments/bits.py
@@ -0,0 +1,129 @@
+from sleekit.codebook import *
+from sleekit.obq import *
+from sleekit.scaling import *
+import matplotlib.pyplot as plt
+import os
+import tqdm
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Analysis of the effect of the number of bits on the error"
+)
+parser.add_argument("dir", type=str, help="Directory containing the weights")
+parser.add_argument("--damp", type=float, default=0.01, help="Hessian dampening")
+parser.add_argument(
+    "--correct-bias",
+    action="store_true",
+    help="Use the hessian with bias correction for scaling and evaluation",
+)
+
+gp = parser.add_argument_group("Scaling")
+gp.add_argument(
+    "--scaling",
+    type=str,
+    choices=["mse", "max", "hessian", "diag", "obq"],
+    default="mse",
+    help="Scaling method to use",
+)
+gp.add_argument(
+    "--grid-size", type=int, default=100, help="Grid size for error minimization"
+)
+gp.add_argument(
+    "--min-factor",
+    type=float,
+    default=0.05,
+    help="Minimum scaling factor for error minimization",
+)
+gp.add_argument(
+    "--max-factor",
+    type=float,
+    default=1.0,
+    help="Maximum scaling factor for error minimization",
+)
+args = parser.parse_args()
+
+roots = sorted(
+    [
+        root
+        for root, dirs, files in sorted(os.walk(args.dir))
+        if "weight.npy" in files and "hessian.npy" in files and "mean.npy" in files
+    ]
+)
+
+bits = [
+    (2, 1),
+    (3, 1.5),
+    (4, 2),
+    (5, 2.3),
+    (7, 2.8),
+    (8, 3),
+    (9, 3.2),
+    (15, 3.9),
+    (16, 4),
+    (32, 5),
+]
+
+msg = "Data\tScaling"
+for sz, b in bits:
+    msg += f"\t{b}-bit"
+print(msg)
+
+it = tqdm.tqdm(roots)
+for root in it:
+    weight = np.load(os.path.join(root, "weight.npy")).astype(np.float32)
+    hessian = np.load(os.path.join(root, "hessian.npy")).astype(np.float32)
+    mean = np.load(os.path.join(root, "mean.npy")).astype(np.float32)
+    remove_dead_values(hessian, weight, pcdamp=args.damp)
+    if args.correct_bias:
+        hessian = remove_input_bias(hessian, mean)
+    name = os.path.relpath(root, args.dir)
+    msg = f"{name}\t{args.scaling}"
+
+    for sz, b in bits:
+        cb = Codebook.uniform(sz, -1, 1)
+        if args.scaling == "mse":
+            sc = compute_min_mse_scaling(
+                weight,
+                cb,
+                grid_size=args.grid_size,
+                min_factor=args.min_factor,
+                max_factor=args.max_factor,
+            )
+        elif args.scaling == "max":
+            sc = compute_non_saturating_scaling(weight, cb)
+        elif args.scaling == "hessian":
+            sc = compute_min_mse_scaling(
+                weight,
+                cb,
+                H=hessian,
+                grid_size=args.grid_size,
+                min_factor=args.min_factor,
+                max_factor=args.max_factor,
+            )
+        elif args.scaling == "diag":
+            sc = compute_min_mse_scaling(
+                weight,
+                cb,
+                H=np.diag(hessian),
+                grid_size=args.grid_size,
+                min_factor=args.min_factor,
+                max=args.max_factor,
+            )
+        elif args.scaling == "obq":
+            sc = compute_min_mse_scaling(
+                weight,
+                cb,
+                H=hessian,
+                obq=True,
+                grid_size=args.grid_size,
+                min_factor=args.min_factor,
+                max_factor=args.max_factor,
+            )
+        else:
+            raise RuntimeError(f"Unknown scaling {args.scaling}")
+
+        quant_weight = quantize_with_scaling(weight, sc, cb, H=hessian)
+        error = quantization_error(weight, quant_weight, H=hessian)
+        msg += f"\t{error}"
+    it.write(msg)
--- results/graph.py
+++ results/graph.py
@@ -4,11 +4,12 @@ from matplotlib.ticker import PercentFormatter
 import pandas
 import numpy as np
 
-for b in [1, 1.5, 2, 3]:
+
+def export_ordering_graph(b):
     try:
         data = pandas.read_csv(f"results/ordering_{b}b.csv", sep="\t")
     except FileNotFoundError:
-        continue
+        return
     diag = [1.0 for d in data["Diag"]]
     diag_err = sorted(data["DiagErr"] / data["Diag"], reverse=True)
     diag_sqerr = sorted(data["DiagSqErr"] / data["Diag"], reverse=True)
@@ -40,11 +41,12 @@ for b in [1, 1.5, 2, 3]:
     # plt.show()
     plt.clf()
 
-for b in [1, 1.5, 2, 3]:
+
+def export_correction_graph(b):
     try:
         data = pandas.read_csv(f"results/correction_{b}b.csv", sep="\t")
     except FileNotFoundError:
-        continue
+        return
     gptq = [1.0 for d in data["GPTQ"]]
     plus_bias = sorted(data["GPTQ+BiasCorrection"] / data["GPTQ"], reverse=True)
     with_bias = sorted(data["GPTQWithBiasCorrection"] / data["GPTQ"], reverse=True)
@@ -75,11 +77,12 @@ for b in [1, 1.5, 2, 3]:
     # plt.show()
     plt.clf()
 
-for b in [1, 1.5, 2, 3]:
+
+def export_compare_graph(b):
     try:
         data = pandas.read_csv(f"results/compare_{b}b.csv", sep="\t")
     except FileNotFoundError:
-        continue
+        return
     standard = [1.0 for d in data["Standard"]]
     correction = sorted(data["Correction"] / data["Standard"], reverse=True)
     scaling = sorted(data["Scaling"] / data["Standard"], reverse=True)
@@ -113,11 +116,12 @@ for b in [1, 1.5, 2, 3]:
     # plt.show()
     plt.clf()
 
-for b in [1, 1.5, 2, 3]:
+
+def export_scaling_graph(b):
     try:
         data = pandas.read_csv(f"results/scaling_{b}b.csv", sep="\t")
     except FileNotFoundError:
-        continue
+        return
     mse = [1.0 for d in data["MSE"]]
     diag = sorted(data["Diag"] / data["MSE"], reverse=True)
     hessian = sorted(data["Hessian"] / data["MSE"], reverse=True)
@@ -150,3 +154,50 @@ for b in [1, 1.5, 2, 3]:
     plt.savefig(f"results/scaling_{b}b.png")
     # plt.show()
     plt.clf()
+
+
+def export_bits_graph():
+    try:
+        data = pandas.read_csv(f"results/bits.csv", sep="\t")
+    except FileNotFoundError:
+        return
+    d3 = [1.0 for d in data["3-bit"]]
+    d2 = sorted(data["2-bit"] / data["3-bit"], reverse=True)
+    d1_5 = sorted(data["1.5-bit"] / data["3-bit"], reverse=True)
+    d1 = sorted(data["1-bit"] / data["3-bit"], reverse=True)
+    geomean_d2 = np.exp(np.mean(np.log(d2)))
+    geomean_d1_5 = np.exp(np.mean(np.log(d1_5)))
+    geomean_d1 = np.exp(np.mean(np.log(d1)))
+    print(f"Bits: 2b x{geomean_d2:.2f}, 1.5b x{geomean_d1_5:.2f}, 1b x{geomean_d1:.2f}")
+
+    plt.title(f"Impact of the number of bits; lower is better")
+    plt.xlabel("Layers")
+    plt.ylabel("Error relative to 3-bit scaling (%)")
+    plt.yscale("log")
+    plt.xlim(left=0, right=len(data) - 1)
+    plt.ylim(bottom=1, top=20)
+
+    yticks = [1, 1.5, 2, 3, 5, 7, 10, 15, 20]
+    plt.gca().set_yticks([])
+    plt.gca().set_yticks([], minor=True)
+    plt.gca().set_yticks(yticks)
+    plt.gca().set_yticklabels([f"x{t}" for t in yticks])
+
+    plt.plot(d2, label="2-bit")
+    plt.plot(d1_5, label="1.5-bit")
+    plt.plot(d1, label="1-bit")
+    plt.legend()
+    plt.savefig(f"results/bits.png")
+    # plt.show()
+    plt.clf()
+
+
+for b in [1, 1.5, 2, 3]:
+    export_ordering_graph(b)
+for b in [1, 1.5, 2, 3]:
+    export_correction_graph(b)
+for b in [1, 1.5, 2, 3]:
+    export_compare_graph(b)
+for b in [1, 1.5, 2, 3]:
+    export_scaling_graph(b)
+export_bits_graph()
