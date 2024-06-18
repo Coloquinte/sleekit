cf1167fa13088359dabf97fcacad087b19f8ba0a
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-06-18 15:54:50 +0100

    Command cleanup

--- export_statistics.py
+++ export_statistics.py
@@ -7,8 +7,18 @@ import torch.nn as nn
 
 from sleekit import Sleekit
 from datautils import *
-from modelutils import *
 import tqdm
+
+
+def find_layers(module, layers=[nn.Conv2d, nn.Linear], name=''):
+    if type(module) in layers:
+        return {name: module}
+    res = {}
+    for name1, child in module.named_children():
+        res.update(find_layers(
+            child, layers=layers, name=name + '.' + name1 if name != '' else name1
+        ))
+    return res
 
 
 def is_bloom(model_name):
