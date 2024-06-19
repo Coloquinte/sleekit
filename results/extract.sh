a4dca0308619fc49bc46c073625e1ce936eaec7e
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-06-19 16:20:44 +0100

    Extraction script

---
+++ results/extract.sh
@@ -0,0 +1,4 @@
+
+python export_statistics.py facebook/opt-125m c4 --path data/opt-125m --numpy --force-fp32
+python export_statistics.py facebook/opt-350m c4 --path data/opt-350m --numpy --force-fp32
+python export_statistics.py bigscience/bloom-560m c4 --path data/bloom-560m --numpy --force-fp32
